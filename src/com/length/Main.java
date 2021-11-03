aa50bf82dd95e892870917a2dd36684c07701758
memories2013 <nshivaprasad41@gmail.com>
2021-11-03 18:16:41 +0530

    Compare The Length Of Two Lines

--- src/com/length/Main.java
+++ src/com/length/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 	
-	public void equals(double x3, double y3, double x4, double y4, double length) {
+	public double equals(double x3, double y3, double x4, double y4, double length) {
+		
 		double length2 = Math.sqrt( Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
 		
 		if (length == length2) {
@@ -12,12 +13,28 @@ public class Main {
 		}
 		else {
 			System.out.println(" The two lines are not equal");
-			System.out.println(" Second line length: " + length2);
+		}
+		
+		return length2;
+	}
+	
+	public void compareTo (double length, double length2) {
+		
+		if (length == length2) {
+			System.out.println(" The two lines are equal in length");
+		}
+		
+		else if (length > length2) {
+			System.out.println(" The first line is greater than the second. ");
+		}
+		
+		else {
+			System.out.println(" The second line is greater than the first. ");
 		}
 	}
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		
 		Main obj = new Main();
 		Scanner read = new Scanner(System.in);
@@ -49,15 +66,17 @@ public class Main {
 		double length = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 		
 		System.out.println(" The first point is : (" + x1 + ", " + y1 + ")");
-		System.out.println(" The second point is : (" + x2 + ", " + y2 + ")");
-		
-		System.out.println(" The length of the line is : " + length);
+		System.out.println(" The second point is : (" + x2 + ", " + y2 + ")");		
 		
 		System.out.println(" The third point is : (" + x3 + ", " + y3 + ")");
 		System.out.println(" The fourth point is : (" + x4 + ", " + y4 + ")");
 		
 		
-		obj.equals(x3, y3, x4, y4, length);
+		double length2 = obj.equals(x3, y3, x4, y4, length);
+		System.out.println(" The length of the line is : " + length);
+		System.out.println(" Second line length: " + length2);
+		
+		obj.compareTo(length, length2);		
 		
 	}
 
