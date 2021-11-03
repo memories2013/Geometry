3456f0275c07cbd7b90cde443b4eb4e359ad165c
memories2013 <nshivaprasad41@gmail.com>
2021-11-03 17:57:26 +0530

    Calculating The Length Of A Line

--- src/com/length/Main.java
+++ src/com/length/Main.java
@@ -3,8 +3,38 @@ package com.length;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println(" in the main class");
+		
+		double neg = Math.floor(Math.random() * 2);		
+		double x1 = Math.floor(Math.random() * 10);
+		
+		if (neg == 1) {
+			x1 *= -1;
+		}
+		
+		neg = Math.floor(Math.random() * 2);
+		double y1 = Math.floor(Math.random() * 10);
+		if (neg == 1) {
+			y1 *= -1;
+		}
+		
+		neg = Math.floor(Math.random() * 2);
+		double x2 = Math.floor(Math.random() * 10);
+		if (neg == 1) {
+			x2 *= -1;
+		}
+		
+		neg = Math.floor(Math.random() * 2);
+		double y2 = Math.floor(Math.random() * 10);
+		if (neg == 1) {
+			y2 *= -1;
+		}
+		
+		double length = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		
+		System.out.println(" The first point is : (" + x1 + ", " + y1 + ")");
+		System.out.println(" The first point is : (" + x2 + ", " + y2 + ")");
+		System.out.println(" The length of the line is : " + length);
 	}
 
 }
